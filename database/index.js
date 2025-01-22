6b50c8bbe0f352ddc0dade4912952d909b0523fc
hendrikekon <hendrikekon25@gmail.com>
2025-01-22 08:21:06 +0700

    change the db to mongodb atlas

--- database/index.js
+++ database/index.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
 const { dbUser, dbPass, dbHost, dbPort, dbName } =require('../app/config')
 
-mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
+// mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
 
+// const db = mongoose.connection;
+
+// module.exports = db;
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.lh18l.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`);
 const db = mongoose.connection;
 
 module.exports = db;
